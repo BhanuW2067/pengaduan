3b4b5c4c1ccb0f8c8ecd831f710b3d129625407e
BhanuW2067 <bhanuwijaksono@gmail.com>
2024-12-09 13:37:51 +0700

    Update db_pengaduan.sql

--- db_pengaduan.sql
+++ db_pengaduan.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 06 Des 2024 pada 14.28
--- Versi server: 10.4.32-MariaDB
--- Versi PHP: 8.2.12
+-- Generation Time: Dec 09, 2024 at 07:37 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,174 +24,143 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `pengaduan`
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` int NOT NULL,
+  `nama_admin` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengaduan`
 --
 
 CREATE TABLE `pengaduan` (
-  `id_pengaduan` int(11) NOT NULL,
+  `id_pengaduan` int NOT NULL,
   `tgl_pengaduan` date NOT NULL,
-  `nik` varchar(16) NOT NULL,
-  `isi_laporan` text NOT NULL,
-  `foto` varchar(255) NOT NULL,
-  `status` enum('0','proses','selesai') NOT NULL
+  `id_user` int NOT NULL,
+  `isi_laporan` text COLLATE utf8mb4_general_ci NOT NULL,
+  `foto` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `status` enum('diterima','proses','selesai') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data untuk tabel `pengaduan`
---
-
-INSERT INTO `pengaduan` (`id_pengaduan`, `tgl_pengaduan`, `nik`, `isi_laporan`, `foto`, `status`) VALUES
-(10, '2024-11-14', '10900', 'bebek', '1731581237.jpg', 'selesai'),
-(11, '2024-11-21', '10200', 'pengaduan', '1732177211.jpg', '0'),
-(12, '2024-11-21', '10200', 'bebek', '1732177265.jpg', 'selesai'),
-(13, '2024-11-22', '10811', 'ambon itam', '1732263288.jpg', '0'),
-(14, '2024-12-05', '10930', 'maling', '1733407144.jpg', 'selesai');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `pengaduan_siswa`
---
-
-CREATE TABLE `pengaduan_siswa` (
-  `nik` varchar(16) NOT NULL,
-  `nama` varchar(35) NOT NULL,
-  `username` varchar(25) NOT NULL,
-  `password` varchar(32) NOT NULL,
-  `telp` varchar(13) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data untuk tabel `pengaduan_siswa`
---
-
-INSERT INTO `pengaduan_siswa` (`nik`, `nama`, `username`, `password`, `telp`) VALUES
-('10101', 'Ahmad Damari', 'mad', 'ab56b4d92b40713acc5af89985d4b786', '08589765579'),
-('10200', 'rangga2', 'rangga', '202cb962ac59075b964b07152d234b70', '123'),
-('10290', 'irsan abidin', 'riruru', '202cb962ac59075b964b07152d234b70', '0895347750219'),
-('10800', 'farel', 'farel', '202cb962ac59075b964b07152d234b70', '123'),
-('10811', 'Alfarizi', 'banuimut', '202cb962ac59075b964b07152d234b70', '123'),
-('10900', 'ahmad', 'ahmad', '202cb962ac59075b964b07152d234b70', '123'),
-('10930', 'Farel Wicaksono', 'wijak', '827ccb0eea8a706c4c34a16891f84e7b', '08589765579'),
-('10990', 'bebek', 'bebek', '202cb962ac59075b964b07152d234b70', '123');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `petugas`
---
-
-CREATE TABLE `petugas` (
-  `id_petugas` int(11) NOT NULL,
-  `nama_petugas` varchar(36) NOT NULL,
-  `username` varchar(25) NOT NULL,
-  `password` varchar(32) NOT NULL,
-  `telp` varchar(13) NOT NULL,
-  `level` enum('admin','petugas') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data untuk tabel `petugas`
---
-
-INSERT INTO `petugas` (`id_petugas`, `nama_petugas`, `username`, `password`, `telp`, `level`) VALUES
-(1, 'petugas', 'petugas', '827ccb0eea8a706c4c34a16891f84e7b', '085150942510', 'petugas'),
-(2, 'admin', 'admin', '202cb962ac59075b964b07152d234b70', '08509457869', 'admin'),
-(3, 'agus', 'agus', '$2y$10$H/Y5UAEZROpdPgGrzZjUKuu1y', '', 'petugas');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `tanggapan`
+-- Table structure for table `tanggapan`
 --
 
 CREATE TABLE `tanggapan` (
-  `id_tanggapan` int(11) NOT NULL,
-  `id_pengaduan` int(11) NOT NULL,
+  `id_tanggapan` int NOT NULL,
+  `id_pengaduan` int NOT NULL,
   `tgl_tanggapan` date NOT NULL,
-  `tanggapan` text NOT NULL,
-  `foto_tanggapan` varchar(255) NOT NULL,
-  `id_petugas` int(11) NOT NULL
+  `tanggapan` text COLLATE utf8mb4_general_ci NOT NULL,
+  `foto_tanggapan` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `id_admin` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int NOT NULL,
+  `nama` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `kelas` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `tgl_daftar` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `tanggapan`
+-- Dumping data for table `user`
 --
 
-INSERT INTO `tanggapan` (`id_tanggapan`, `id_pengaduan`, `tgl_tanggapan`, `tanggapan`, `foto_tanggapan`, `id_petugas`) VALUES
-(12, 12, '2024-12-05', 'udah ya', '1732177741_673eef4dab3e9.jpg', 2),
-(13, 10, '2024-11-21', 'udah', '1732177883_673eefdb028a0.jpg', 2),
-(14, 14, '2024-12-05', 'proses gan', '1733407380_6751b2946ce85.jpg', 2);
+INSERT INTO `user` (`id_user`, `nama`, `kelas`, `email`, `password`, `tgl_daftar`) VALUES
+(1, 'Bayu Wijak', '11', 'bayu@gmail.com', '202cb962ac59075b964b07152d234b70', '2024-12-09 05:16:53');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `pengaduan`
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indexes for table `pengaduan`
 --
 ALTER TABLE `pengaduan`
   ADD PRIMARY KEY (`id_pengaduan`),
-  ADD KEY `nik` (`nik`);
+  ADD KEY `id_user` (`id_user`);
 
 --
--- Indeks untuk tabel `pengaduan_siswa`
---
-ALTER TABLE `pengaduan_siswa`
-  ADD PRIMARY KEY (`nik`);
-
---
--- Indeks untuk tabel `petugas`
---
-ALTER TABLE `petugas`
-  ADD PRIMARY KEY (`id_petugas`);
-
---
--- Indeks untuk tabel `tanggapan`
+-- Indexes for table `tanggapan`
 --
 ALTER TABLE `tanggapan`
   ADD PRIMARY KEY (`id_tanggapan`),
   ADD KEY `id_pengaduan` (`id_pengaduan`),
-  ADD KEY `id_petugas` (`id_petugas`);
+  ADD KEY `id_admin` (`id_admin`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `pengaduan`
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `pengaduan`
 --
 ALTER TABLE `pengaduan`
-  MODIFY `id_pengaduan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id_pengaduan` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
--- AUTO_INCREMENT untuk tabel `petugas`
---
-ALTER TABLE `petugas`
-  MODIFY `id_petugas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT untuk tabel `tanggapan`
+-- AUTO_INCREMENT for table `tanggapan`
 --
 ALTER TABLE `tanggapan`
-  MODIFY `id_tanggapan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id_tanggapan` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
 --
 
 --
--- Ketidakleluasaan untuk tabel `pengaduan`
+-- Constraints for table `pengaduan`
 --
 ALTER TABLE `pengaduan`
-  ADD CONSTRAINT `pengaduan_ibfk_1` FOREIGN KEY (`nik`) REFERENCES `pengaduan_siswa` (`nik`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `pengaduan_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Ketidakleluasaan untuk tabel `tanggapan`
+-- Constraints for table `tanggapan`
 --
 ALTER TABLE `tanggapan`
   ADD CONSTRAINT `tanggapan_ibfk_1` FOREIGN KEY (`id_pengaduan`) REFERENCES `pengaduan` (`id_pengaduan`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `tanggapan_ibfk_2` FOREIGN KEY (`id_petugas`) REFERENCES `petugas` (`id_petugas`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `tanggapan_ibfk_2` FOREIGN KEY (`id_admin`) REFERENCES `admin` (`id_admin`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
