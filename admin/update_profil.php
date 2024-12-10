fe78a4ded12fad45941662c579be011824c18247
BhanuW2067 <bhanuwijaksono@gmail.com>
2024-12-10 12:16:03 +0700

    Update update_profil.php

--- admin/update_profil.php
+++ admin/update_profil.php
@@ -1,42 +1,41 @@
 <?php
-require_once('cek_akses.php');
+session_start();
 require_once('../config/database.php');
 
+if(!isset($_SESSION['id_admin'])) {
+    header("Location: ../login_admin.php");
+    exit();
+}
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $id_petugas = $_SESSION['id_petugas'];
-    $nama_petugas = $_POST['nama_petugas'];
-    $username = $_POST['username'];
+    $id_admin = $_SESSION['id_admin'];
+    $nama_admin = $_POST['nama_admin'];
+    $email = $_POST['email'];
     $password = $_POST['password'];
     $konfirmasi_password = $_POST['konfirmasi_password'];
 
-    // Validasi username unik
-    $check_username = "SELECT id_petugas FROM petugas WHERE username = '$username' AND id_petugas != $id_petugas";
+    $check_username = "SELECT id_admin FROM admin WHERE nama_admin = '$nama_admin' AND id_admin != $id_admin";
     $result = $conn->query($check_username);
     if ($result->num_rows > 0) {
         header("Location: profil.php?error=Username sudah digunakan");
         exit();
     }
 
-    // Jika password diisi
     if (!empty($password)) {
-        // Validasi konfirmasi password
         if ($password !== $konfirmasi_password) {
             header("Location: profil.php?error=Password dan konfirmasi password tidak cocok");
             exit();
         }
 
-        // Update dengan password baru
         $password_hash = md5($password);
-        $query = "UPDATE petugas SET nama_petugas = '$nama_petugas', username = '$username', password = '$password_hash' WHERE id_petugas = $id_petugas";
+        $query = "UPDATE admin SET nama_admin = '$nama_admin', email ='$email', password = '$password_hash' WHERE id_admin = $id_admin";
     } else {
-        // Update tanpa password
-        $query = "UPDATE petugas SET nama_petugas = '$nama_petugas', username = '$username' WHERE id_petugas = $id_petugas";
+        $query = "UPDATE admin SET nama_admin = '$nama_admin' WHERE id_admin = $id_admin";
     }
 
     if ($conn->query($query)) {
-        // Update session
-        $_SESSION['nama_petugas'] = $nama_petugas;
-        $_SESSION['username'] = $username;
+        $_SESSION['nama_admin'] = $nama_admin;
+        $_SESSION['email'] = $email;
         
         header("Location: profil.php?success=Profil berhasil diupdate");
     } else {
